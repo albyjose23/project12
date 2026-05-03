1f2ceff3624364bc1e980dc13ae41648c3558a41
albyjose23 <albyjose30@gmail.com>
2026-05-03 21:25:27 +0530

    describe the update

--- db/migrate/20260503090000_add_import_batch_fields_to_questions.rb
+++
@@ -1,7 +0,0 @@
-class AddImportBatchFieldsToQuestions < ActiveRecord::Migration[8.1]
-  def change
-    add_column :questions, :import_batch_id, :string
-    add_column :questions, :import_source_name, :string
-    add_index :questions, :import_batch_id
-  end
-end
